209bc11bebbf64988bf8182c683129f52aed6ae1
Shadows_of_Fire <Bward7864@gmail.com>
2017-06-16 03:25:33 -0400

    update helper

--- src/main/java/shadows/plants/util/RecipeHelper.java
+++ src/main/java/shadows/plants/util/RecipeHelper.java
@@ -1,11 +1,6 @@
 package shadows.plants.util;
 
-import java.util.HashMap;
-import javax.annotation.Nonnull;
-
-import com.google.common.collect.Maps;
 import net.minecraft.block.Block;
-import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
@@ -14,11 +9,9 @@ import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.item.crafting.ShapelessRecipes;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.common.registry.IForgeRegistryEntry;
 import net.minecraftforge.oredict.OreIngredient;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class RecipeHelper {
@@ -31,29 +24,40 @@ public class RecipeHelper {
 	 * This adds the recipe to the list of crafting recipes.  Since who cares about names, it adds it as recipesX, where X is the current recipe you are adding.
 	 */
 	public static void addRecipe(int j, IRecipe rec) {
+	    if(rec.getRegistryName() == null)
 		GameRegistry.register(rec.setRegistryName(new ResourceLocation(MODID, "recipes" + j)));
+	    else GameRegistry.register(rec);
 	}
 
 	/*
 	 * This adds the recipe to the list of crafting recipes.  Cares about names.
 	 */
 	public static void addRecipe(String name, IRecipe rec) {
+	    if(rec.getRegistryName() == null)
 		GameRegistry.register(rec.setRegistryName(new ResourceLocation(MODID, name)));
+	    else GameRegistry.register(rec);
 	}
 
 	/*
 	 * This adds a shaped recipe to the list of crafting recipes, using the forge format.
 	 */
 	public static void addOldShaped(ItemStack output, Object... input) {
-		addRecipe(j++, new FixedShapedOreRecipe(new ResourceLocation(MODID, "recipes" + j), output, input));
+		addRecipe(j++, new ShapedOreRecipe(new ResourceLocation(MODID, "recipes" + j), output, input));
 	}
 
 	/*
 	 * This adds a shaped recipe to the list of crafting recipes, using the forge format, with a custom group.
 	 */
 	public static void addOldShaped(String group, ItemStack output, Object... input) {
-		addRecipe(j++, new FixedShapedOreRecipe(new ResourceLocation(MODID, group), output, input));
+		addRecipe(j++, new ShapedOreRecipe(new ResourceLocation(MODID, group), output, input));
 	}
+	
+	   /*
+     * This adds a shaped recipe to the list of crafting recipes, using the forge format, with a custom group.
+     */
+    public static void addOldShaped(String name, String group, ItemStack output, Object... input) {
+        addRecipe(j++, new ShapedOreRecipe(new ResourceLocation(MODID, group), output, input).setRegistryName(MODID, name));
+    }
 
 	/*
 	 * This adds a shapeless recipe to the list of crafting recipes, using the forge format.
@@ -202,199 +206,4 @@ public class RecipeHelper {
 		}
 		return inputL;
 	}
-
-	//This is ShapedOreRecipe modified to actually work until forge re-fixes it in an update.
-	public static class FixedShapedOreRecipe extends IForgeRegistryEntry.Impl<IRecipe> implements IRecipe {
-		//Added in for future ease of change, but hard coded for now.
-		public static final int MAX_CRAFT_GRID_WIDTH = 3;
-		public static final int MAX_CRAFT_GRID_HEIGHT = 3;
-
-		@Nonnull
-		protected ItemStack output = ItemStack.EMPTY;
-		protected NonNullList<Ingredient> input = null;
-		protected int width = 0;
-		protected int height = 0;
-		protected boolean mirrored = true;
-		protected ResourceLocation group;
-
-		public FixedShapedOreRecipe(ResourceLocation group, Block result, Object... recipe) {
-			this(group, new ItemStack(result), recipe);
-		}
-
-		public FixedShapedOreRecipe(ResourceLocation group, Item result, Object... recipe) {
-			this(group, new ItemStack(result), recipe);
-		}
-
-		public FixedShapedOreRecipe(ResourceLocation group, @Nonnull ItemStack result, Object... recipe) {
-			this.group = group;
-			output = result.copy();
-
-			String shape = "";
-			int idx = 0;
-
-			if (recipe[idx] instanceof Boolean) {
-				mirrored = (Boolean) recipe[idx];
-				if (recipe[idx + 1] instanceof Object[]) {
-					recipe = (Object[]) recipe[idx + 1];
-				} else {
-					idx = 1;
-				}
-			}
-
-			if (recipe[idx] instanceof String[]) {
-				String[] parts = ((String[]) recipe[idx++]);
-
-				for (String s : parts) {
-					width = s.length();
-					shape += s;
-				}
-
-				height = parts.length;
-			} else {
-				while (recipe[idx] instanceof String) {
-					String s = (String) recipe[idx++];
-					shape += s;
-					width = s.length();
-					height++;
-				}
-			}
-
-			if (width * height != shape.length()) {
-				String ret = "Invalid shaped ore recipe: ";
-				for (Object tmp : recipe) {
-					ret += tmp + ", ";
-				}
-				ret += output;
-				throw new RuntimeException(ret);
-			}
-
-			HashMap<Character, Ingredient> itemMap = Maps.newHashMap();
-
-			for (; idx < recipe.length; idx += 2) {
-				Character chr = (Character) recipe[idx];
-				Object in = recipe[idx + 1];
-
-				if (in instanceof ItemStack) {
-					itemMap.put(chr, Ingredient.fromStacks(((ItemStack) in).copy()));
-				} else if (in instanceof Item) {
-					itemMap.put(chr, Ingredient.fromStacks(new ItemStack((Item) in)));
-				} else if (in instanceof Block) {
-					itemMap.put(chr,
-							Ingredient.fromStacks(new ItemStack((Block) in)));
-				} else if (in instanceof String) {
-					itemMap.put(chr, new OreIngredient((String) in));
-				} else if (in instanceof Ingredient) {
-					itemMap.put(chr, (Ingredient) in);
-				} else {
-					String ret = "Invalid shaped ore recipe: ";
-					for (Object tmp : recipe) {
-						ret += tmp + ", ";
-					}
-					ret += output;
-					throw new RuntimeException(ret);
-				}
-			}
-
-			this.input = NonNullList.withSize(width * height, Ingredient.EMPTY);
-			int x = 0;
-			for (char chr : shape.toCharArray()) {
-				if (itemMap.get(chr) != null)
-					input.set(x, itemMap.get(chr));
-				x++;
-			}
-		}
-
-		/**
-		 * Returns an Item that is the result of this recipe
-		 */
-		@Override
-		@Nonnull
-		public ItemStack getCraftingResult(@Nonnull InventoryCrafting var1) {
-			return output.copy();
-		}
-
-		@Override
-		@Nonnull
-		public ItemStack getRecipeOutput() {
-			return output;
-		}
-
-		/**
-		 * Used to check if a recipe matches current crafting inventory
-		 */
-		@Override
-		public boolean matches(InventoryCrafting inv, World world) {
-			for (int x = 0; x <= MAX_CRAFT_GRID_WIDTH - width; x++) {
-				for (int y = 0; y <= MAX_CRAFT_GRID_HEIGHT - height; ++y) {
-					if (checkMatch(inv, x, y, false)) {
-						return true;
-					}
-
-					if (mirrored && checkMatch(inv, x, y, true)) {
-						return true;
-					}
-				}
-			}
-
-			return false;
-		}
-
-		protected boolean checkMatch(InventoryCrafting inv, int startX, int startY, boolean mirror) {
-			for (int x = 0; x < MAX_CRAFT_GRID_WIDTH; x++) {
-				for (int y = 0; y < MAX_CRAFT_GRID_HEIGHT; y++) {
-					int subX = x - startX;
-					int subY = y - startY;
-					Ingredient target = null;
-
-					if (subX >= 0 && subY >= 0 && subX < width && subY < height) {
-						if (mirror) {
-							target = input.get(width - subX - 1 + subY * width);
-						} else {
-							target = input.get(subX + subY * width);
-						}
-					}
-
-					if (!target.apply(inv.getStackInRowAndColumn(x, y))) {
-						return false;
-					}
-				}
-			}
-
-			return true;
-		}
-
-		public FixedShapedOreRecipe setMirrored(boolean mirror) {
-			mirrored = mirror;
-			return this;
-		}
-
-		public NonNullList<Ingredient> func_192400_c() {
-			return this.input;
-		}
-
-		@Override
-		public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) //getRecipeLeftovers
-		{
-			return ForgeHooks.defaultRecipeGetRemainingItems(inv);
-		}
-
-		public int getWidth() {
-			return width;
-		}
-
-		public int getHeight() {
-			return height;
-		}
-		
-		@Override
-		public String getGroup() {
-			return this.group.toString();
-		}
-		
-		@Override
-		public boolean canFit(int width, int height) {
-			return width >= this.width && height >= this.height;
-		}
-	}
-
 }
