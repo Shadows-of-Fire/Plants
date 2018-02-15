b215ebd1544bd8317fdf3bfccc96959246427b8e
Shadows_of_Fire <Bward7864@gmail.com>
2018-02-15 02:58:42 -0500

    special case fruitybois

--- src/main/java/shadows/plants2/block/BlockFlowerpot.java
+++ src/main/java/shadows/plants2/block/BlockFlowerpot.java
@@ -89,6 +89,7 @@ public class BlockFlowerpot extends BlockFlowerPot implements IHasModel, IItemBl
 			IBlockState toUse = Block.getBlockFromItem(held.getItem()).getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, held.getMetadata(), player, hand);
 			if (toUse.getBlock() == Blocks.AIR && held.getItem() instanceof IPlantable) toUse = ((IPlantable) held.getItem()).getPlant(world, pos);
 			if (toUse.getBlock() != this && (toUse.getBlock() == Blocks.AIR || toUse.getBlock() instanceof ITileEntityProvider || toUse.getBlock().hasTileEntity(toUse))) return false;
+			if(BlockEnumHarvestBush.FRUIT.equals(toUse.getBlock().getBlockState().getProperty("fruit"))) toUse = toUse.withProperty(BlockEnumHarvestBush.FRUIT, true);
 			pot.setState(toUse);
 			ItemStack copy = held.copy();
 			copy.setCount(1);
@@ -178,6 +179,13 @@ public class BlockFlowerpot extends BlockFlowerPot implements IHasModel, IItemBl
 		TileFlowerpot t = getTileEntity(world, pos);
 		if(t != null && !t.getStack().isEmpty()) return t.getStack();
 		return new ItemStack(this);
+	}
+	
+	@Override
+	public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) {
+		TileFlowerpot t = getTileEntity(world, pos);
+		if(t == null) return;
+		t.getState().getBlock().randomDisplayTick(t.getState(), world, pos, rand);
 	}
 
 	public static class UnlistedStateProperty implements IUnlistedProperty<IBlockState> {
