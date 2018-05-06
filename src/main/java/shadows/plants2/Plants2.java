02650bc2ff9c8e9b6761ed6c503e916f73b13b91
Shadows_of_Fire <Bward7864@gmail.com>
2018-05-06 00:01:05 -0400

    dependy

--- src/main/java/shadows/plants2/Plants2.java
+++ src/main/java/shadows/plants2/Plants2.java
@@ -39,7 +39,7 @@ public class Plants2 {
 	public static final String MODID = "plants2";
 	public static final String MODNAME = "Plants";
 	public static final String VERSION = "2.8.3";
-	public static final String DEPS = "required-after:placebo@[1.3.2,);after:botania;after:forestry;after:inspirations";
+	public static final String DEPS = "required-after:placebo@[1.3.4,);after:botania;after:forestry;after:inspirations";
 
 	@Instance
 	public static Plants2 INSTANCE;
