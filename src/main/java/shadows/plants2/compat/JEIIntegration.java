eed2797ba3b8a12b4edddb360e5b5942f2b13913
Brennan Ward <Bward7864@gmail.com>
2018-08-22 13:36:07 -0400

    update jei api

--- src/main/java/shadows/plants2/compat/JEIIntegration.java
+++ src/main/java/shadows/plants2/compat/JEIIntegration.java
@@ -3,6 +3,7 @@ package shadows.plants2.compat;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.ingredients.VanillaTypes;
 import net.minecraft.item.ItemStack;
 import shadows.plants2.init.ModRegistry;
 
@@ -11,10 +12,10 @@ public class JEIIntegration implements IModPlugin {
 
 	@Override
 	public void register(IModRegistry registry) {
-		registry.addIngredientInfo(new ItemStack(ModRegistry.BREWING_CAULDRON), ItemStack.class, "info.plants2.cauldron");
-		registry.addIngredientInfo(new ItemStack(ModRegistry.FLOWERPOT), ItemStack.class, "info.plants2.flowerpot");
-		registry.addIngredientInfo(new ItemStack(ModRegistry.JAR), ItemStack.class, "info.plants2.jar");
-		registry.addIngredientInfo(new ItemStack(ModRegistry.PLANTBALL), ItemStack.class, "info.plants2.plantball", "", "", "info.plants2.plantball.2");
+		registry.addIngredientInfo(new ItemStack(ModRegistry.BREWING_CAULDRON), VanillaTypes.ITEM, "info.plants2.cauldron");
+		registry.addIngredientInfo(new ItemStack(ModRegistry.FLOWERPOT), VanillaTypes.ITEM, "info.plants2.flowerpot");
+		registry.addIngredientInfo(new ItemStack(ModRegistry.JAR), VanillaTypes.ITEM, "info.plants2.jar");
+		registry.addIngredientInfo(new ItemStack(ModRegistry.PLANTBALL), VanillaTypes.ITEM, "info.plants2.plantball", "", "", "info.plants2.plantball.2");
 	}
 
 }
